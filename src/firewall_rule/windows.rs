11de8c6e53a9e628c66c4b77da6f1e9c81a96f85
lhenry-dev <lhenry.dev1@gmail.com>
2026-02-17 22:51:19 +0100

    fix: upgrade windows_firewall version

--- src/firewall_rule/windows.rs
+++ src/firewall_rule/windows.rs
@@ -1,7 +1,13 @@
+use std::collections::HashSet;
+use std::net::IpAddr;
+
+use windows_firewall::Address;
+use windows_firewall::Port;
+use windows_firewall::PortKeyword;
+
 use crate::firewall_rule::FirewallRule;
 use crate::firewall_rule::FirewallRuleError;
 use crate::firewall_rule::FirewallRuleProvider;
-use windows_firewall::WindowsFirewallRule;
 
 /// Windows implementation of the firewall rule provider.
 #[derive(Debug)]
@@ -15,8 +21,8 @@ impl FirewallRuleProvider for WindowsFirewallProvider {
     }
 }
 
-impl From<&WindowsFirewallRule> for FirewallRule {
-    fn from(rule: &WindowsFirewallRule) -> Self {
+impl From<&windows_firewall::FirewallRule> for FirewallRule {
+    fn from(rule: &windows_firewall::FirewallRule) -> Self {
         Self {
             os: Some("windows".to_string()),
             name: rule.name().to_string(),
@@ -27,10 +33,10 @@ impl From<&WindowsFirewallRule> for FirewallRule {
             application_name: rule.application_name().as_deref().map(ToString::to_string),
             service_name: rule.service_name().as_deref().map(ToString::to_string),
             protocol: rule.protocol().map(|p| format!("{p:?}")),
-            local_ports: rule.local_ports().clone(),
-            remote_ports: rule.remote_ports().clone(),
-            local_addresses: rule.local_addresses().clone(),
-            remote_addresses: rule.remote_addresses().clone(),
+            local_ports: windows_ports_to_unit_list(rule.local_ports()),
+            remote_ports: windows_ports_to_unit_list(rule.remote_ports()),
+            local_addresses: windows_addresses_to_unit_list(rule.local_addresses()),
+            remote_addresses: windows_addresses_to_unit_list(rule.remote_addresses()),
             icmp_types_and_codes: rule
                 .icmp_types_and_codes()
                 .as_deref()
@@ -45,4 +51,59 @@ impl From<&WindowsFirewallRule> for FirewallRule {
             edge_traversal: *rule.edge_traversal(),
         }
     }
+}
+
+pub fn windows_ports_to_unit_list(ports: &Option<HashSet<Port>>) -> Option<HashSet<u16>> {
+    let mut result = HashSet::new();
+
+    if let Some(set) = ports {
+        for port in set {
+            match port {
+                Port::Any => return None,
+                Port::Port(p) => {
+                    result.insert(*p);
+                }
+                Port::Range(_r) => {}
+                Port::Keyword(k) => match k {
+                    PortKeyword::Rpc => {}
+                    PortKeyword::RpcEpmap => {
+                        result.insert(135);
+                    }
+                    PortKeyword::IpHttps => {
+                        result.insert(443);
+                    }
+                    PortKeyword::Ply2Disc => {
+                        result.insert(1900);
+                    }
+                    PortKeyword::Teredo => {
+                        result.insert(3544);
+                    }
+                },
+            }
+        }
+    }
+
+    Some(result)
+}
+
+pub fn windows_addresses_to_unit_list(
+    addresses: &Option<HashSet<Address>>,
+) -> Option<HashSet<IpAddr>> {
+    let mut result = HashSet::new();
+
+    if let Some(set) = addresses {
+        for addr in set {
+            match addr {
+                Address::Any => return None,
+                Address::Ip(ip) => {
+                    result.insert(*ip);
+                }
+                Address::Cidr(_net) => {}
+                Address::Range(_range) => {}
+                Address::Keyword(_) => {}
+            }
+        }
+    }
+
+    Some(result)
 }
